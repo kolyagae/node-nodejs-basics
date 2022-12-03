4c5611a8926b87372eda672b710dc696d96e361f
EVGEN <k.e.l@tut.by>
2022-12-03 19:52:14 +0300

    feat: complete create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filesDir = `${__dirname}/files/`;
+    const newFile = "fresh.txt";
+    const files = await fs.readdir(filesDir);
+
+    if (files.includes(newFile)) {
+      throw new Error("FS operation failed");
+    }
+
+    fs.writeFile(`${filesDir}${newFile}`, "I am fresh and young");
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await create();
