10bb81aa9f519a98f41aebb4f48f9f95b416b214
EVGEN <k.e.l@tut.by>
2022-12-04 20:36:06 +0300

    refactor: refactor create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,24 +1,18 @@
-import fs, { constants } from "node:fs/promises";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import fs from "node:fs/promises";
 
 const create = async () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  const pathDir = `${__dirname}/files/`;
-  const newFile = "fresh.txt";
-  const pathFile = `${pathDir}${newFile}`;
-  const files = await fs.readdir(pathDir);
+  const newFilePath = new URL("./files/fresh.txt", import.meta.url);
+  const dirPath = new URL("./files", import.meta.url);
+  const dir = await fs.readdir(dirPath);
 
   try {
-    if (files.includes(newFile)) {
+    if (dir.includes("fresh.txt")) {
       throw new Error("FS operation failed");
     }
 
-    fs.writeFile(`${pathFile}`, "I am fresh and young");
-    console.log("File created successfully!");
+    fs.writeFile(newFilePath, "I am fresh and young");
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
   }
 };
 
