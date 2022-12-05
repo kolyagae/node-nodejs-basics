c5d64be52b38a59f7c70b7f50a5f663681d05999
EVGEN <k.e.l@tut.by>
2022-12-05 19:51:41 +0300

    feat: complete zip:compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import zlib from "node:zlib";
+import stream from "node:stream";
+import fs from "node:fs";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const sourcePath = new URL("./files/fileToCompress.txt", import.meta.url);
+  const destinationPath = new URL("./files/archive.gz", import.meta.url);
+  const source = fs.createReadStream(sourcePath);
+  const destination = fs.createWriteStream(destinationPath);
+
+  stream.pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
