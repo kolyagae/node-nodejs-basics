864b230d94ec783cd3263563eb505ff4979f870f
EVGEN <k.e.l@tut.by>
2022-12-05 12:16:31 +0300

    feat: complete list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = new URL("./files", import.meta.url);
+
+  try {
+    const files = await fs.readdir(dirPath).catch(() => {
+      throw new Error("FS operation failed");
+    });
+    console.log(files.filter((file) => file.split(".").length > 1));
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await list();
