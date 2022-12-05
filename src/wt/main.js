488b1eee245c4e08efadebcbe8122d34b4da5552
EVGEN <k.e.l@tut.by>
2022-12-05 22:14:22 +0300

    add: complete tasks: cp.js, wt: main.js, worker.js, zip: decompress

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import os from "node:os";
+import worker from "node:worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const path = new URL("./worker.js", import.meta.url);
+  const cpus = os.cpus();
+  const workerData = cpus.map(
+    (el, i) =>
+      new Promise((resolve, reject) => {
+        new worker.Worker(path, { workerData: 10 + i })
+          .on("message", (message) => resolve(message))
+          .on("error", (message) => reject(message));
+      })
+  );
+
+  const result = await Promise.allSettled(workerData);
+
+  console.log(
+    result.map((el) => {
+      return {
+        status: el.status === "fulfilled" ? "resolved" : "error",
+        data: el.status === "fulfilled" ? el.value : null,
+      };
+    })
+  );
 };
 
 await performCalculations();
