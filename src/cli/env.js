516fa2233d201dbce8bb3050771ecee0aa5f3cf7
EVGEN <k.e.l@tut.by>
2022-12-05 15:41:21 +0300

    feat: complete env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const list = [];
+  Object.entries(process.env)
+    .filter((el) => el[0].startsWith("RSS_"))
+    .forEach((el) => list.push(`${el[0]}=${el[1]}`));
+  const result = list.join("; ");
+  console.log(result);
 };
 
 parseEnv();
